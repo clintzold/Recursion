c974fed515b5a2819d2e9c1b19973563ec097d08
Clint Zold <214383878+clintzold@users.noreply.github.com>
2025-09-05 08:32:30 -0600

    Create iterating and recursive fib methods

---
+++ recursion.rb
@@ -0,0 +1,21 @@
+#iterative fibonacci
+require 'pry-byebug' 
+
+def iterate_fib(n)
+  fib = [0, 1]
+  (n - 1).times do
+    fib << (fib[-1] + fib[-2])
+  end
+return fib[-1]
+end
+
+def recursive_fib(n)
+  if n < 2
+    return n
+  else
+    n = recursive_fib(n - 1) + recursive_fib(n - 2)
+  end
+  
+end
+binding.pry
+put 'hey'
